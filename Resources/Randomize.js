85d96b5b47bce02c384565c8600cd9d86a5e8d77
IgnacioVillanueva <ivillanueva@itba.edu.ar>
2018-11-15 00:40:24 -0300

    Random change

--- Resources/Randomize.js
+++ Resources/Randomize.js
@@ -1,6 +1,12 @@
+function hashCode(s, m) {
+    for(var i = 0, h = 0; i < s.length; i++)
+        h += s.charCodeAt(i);
+    return (h % m) + 1;
+}
+
 window.onload = function () {
     var imgs = document.getElementsByName("badge-img");
     imgs.forEach(function (elem) {
-        elem.src = "Resources/Badges/Badge_" + Math.floor((Math.random() * 9) + 1) + ".png";
+        elem.src = "Resources/Badges/Badge_" + hashCode(elem.nextElementSibling.innerHTML, 9) + ".png";
     });
 }
