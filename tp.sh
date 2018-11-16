aac027c93552473e73032287dff3d6e38d1eb588
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-11-16 15:32:30 -0300

    Ahora esta re fachero el programa

--- tp.sh
+++ tp.sh
@@ -6,27 +6,40 @@ QUERY="./SOquery.xq"
 XSLT="./create_page.xsl"
 
 #Ask for the user ID
-echo "Input user ID:"
+printf "Input user ID: "
 
 read userID
 
 #Ask for the number of comments until is valid
 #---------------------------------------------
-echo "Input number of comments:"
+printf "Input number of comments: "
 
 read NComments
 
 #Use regexp to make sure its an integer 
 while ! [[ "$NComments" =~ ^[0-9]+$ ]] ; do
-	echo "Please input a valid number of comments (an integer greater than or equal to 0):"
+	printf "Please input a valid number of comments (an integer greater than or equal to 0): "
 
 	read NComments
 done
 #---------------------------------------------
 
-echo "Running Query..."
+printf "\nRunning Query...\n\n"
 java net.sf.saxon.Query userID=$userID n=$NComments $QUERY -o:intermediate.xml
-echo "Running Transformation..."
-java net.sf.saxon.Transform -s:intermediate.xml -xsl:$XSLT -o:output.html
-echo "HTML file successfuly created!"
+if [ $? -eq 0 ]; then
+	printf "Query completed successfuly!\n\n"
+	sleep 2s
+	printf "Running Transformation...\n\n"
+	java net.sf.saxon.Transform -s:intermediate.xml -xsl:$XSLT -o:output.html
+	if [ $? -eq 0 ]; then
+		printf "Transformation completed successfuly!\n\n"
+		sleep 2s
+		printf "HTML file successfuly created!\n\n"
+		sleep 2s
+	else
+		printf "Error while producing Transformation, exiting now."
+	fi
+else
+	printf "Error while producing Query, exiting now."
+fi
 
